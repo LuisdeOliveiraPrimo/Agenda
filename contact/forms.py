75d6fac40d51bf860d9c11ea5b7dbf1beabcb566
LuisOlioveira <luis.primo@gmail.com>
2025-05-21 21:22:00 -0300

    Login User

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,8 @@
-from django.core.paginator import Paginator
 from django import forms
 from django.core.exceptions import ValidationError
-from contact.models import Contact
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 
@@ -98,7 +98,35 @@ class ContactForms(forms.ModelForm):
                 )
                 self.add_error('phone', msg)
 
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
 
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
+    
 
         
 
